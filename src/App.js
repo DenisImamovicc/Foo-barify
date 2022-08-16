f392f749ab1957cc5787afeb1afa9e814986c409
DenisImamovicc <Danielarbete@protonmail.com>
2022-08-16 12:39:42 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">'
+    <h1 className='text-center'>Text-replacer</h1>
+      <Form className="Form">
+          <Form.Label>Enter content of file here:</Form.Label>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control as="textarea" placeholder="" className='textarea'/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <h2 className='text-center'>Results:</h2>
+      <div className='resultarea'>
+    
+      </div>
     </div>
   );
 }
