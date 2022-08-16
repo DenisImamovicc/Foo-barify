bc2cfe6822cc6d1f4b6f070657ff09383dae9228
DenisImamovicc <Danielarbete@protonmail.com>
2022-08-16 15:34:50 +0200

    Add textarea form to take input data and store in a state var.Display modified data on another teaxtarea below

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [inputData, setinputData] = useState("")
+
+  function ChangeInputData(input) {
+    setinputData(()=>input)
+  }
+
+
+  function HandleSubmit(event) {
+    event.preventDefault()
+    ChangeInputData(event.target[0].value)
+    // console.log(event.target[0].value);
+  }
+
+  // useEffect(() => {
+  //   console.log(inputData);
+  // }, [inputData])
+  
+
+
   return (
     <div className="App">'
     <h1 className='text-center'>Text-replacer</h1>
-      <Form className="Form">
+      <Form className="Form" onSubmit={HandleSubmit}>
           <Form.Label>Enter content of file here:</Form.Label>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Control as="textarea" placeholder="" className='textarea'/>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" value="submit">
           Submit
         </Button>
       </Form>
       <h2 className='text-center'>Results:</h2>
-      <div className='resultarea'>
-    
-      </div>
+      <Form.Control as="textarea" placeholder="" className='textarea' value={inputData} readOnly/>
+
     </div>
   );
 }
